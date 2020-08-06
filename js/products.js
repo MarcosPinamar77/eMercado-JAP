f3ea2d65899dbc3c94d1b72328b6938870b20ee9
Marcos Viera <marcos.viera.1899@gmail.com>
2020-08-06 10:57:48 -0300

    Entrega 1. Trabajando sobre login.html, products.html  y products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+
+function showProducts(array) {
+    let htmlContentToAppend = "";
+
+    for(item in array){
+        let product = array[item];
+      htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.imgSrc}"  class="img-thumbnail">
+                </div>
+                        <div class="col">
+                            <h4>${product.name}</h4>
+                            <p>${product.description}</p>
+                            <p class="text-muted">${product.cost} usd</p>
+                        </div> 
+                    </div> 
+            </div>
+        </div>
+      `  
+    
+    }
+   document.getElementById('product-container').innerHTML = htmlContentToAppend; 
+
+}
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL)
+        .then(response => {
+            if (response.status === "ok") {
+                showProducts(response.data)
+            }
+        })
 
 });
