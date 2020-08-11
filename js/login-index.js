b61b1d0d87f7c0581be581be678612b082b1e88a
Marcos Viera <marcos.viera.1899@gmail.com>
2020-08-11 09:50:41 -0300

    Nueva logica para redireccionar de index a login

--- js/login-index.js
+++ js/login-index.js
@@ -1,15 +1,7 @@
 const previousPage = document.referrer;
 
-//Tener en cuenta que si trabajo desde local host tengo que cambiar /eMercado-JAP/ por login.html
-if (!previousPage.includes('/eMercado-JAP/')) {
+//Tener en cuenta que si trabajo desde github tengo que cambiar /eMercado-JAP/ por login.html
+if (!previousPage.includes('login.html') && !previousPage.includes('categories.html') && !previousPage.includes('cart.html') && !previousPage.includes('sell.html') && !previousPage.includes('products.html') && !previousPage.includes('index.html')) {
     location.href = 'login.html';
 }
-console.log(document.referrer)
-/*
-const previousPag = document.referrer;
-let prevUrl = document.referrer;
-if(prevUrl.indexOf(window.location.host) !== -1) {
-// Ir a la página anterior
-window.history.back();
-}
-*/
+
