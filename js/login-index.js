d739f0b096a695c69f41395d061c2783c92ab343
Marcos Viera <marcos.viera.1899@gmail.com>
2020-09-05 23:14:28 -0300

    Cambiando menú de navegación

--- js/login-index.js
+++ js/login-index.js
@@ -1,7 +1,7 @@
 const previousPage = document.referrer;
 
 //Tener en cuenta que si trabajo desde github tengo que cambiar /eMercado-JAP/ por login.html
-if (!previousPage.includes('login.html') && !previousPage.includes('categories.html') && !previousPage.includes('cart.html') && !previousPage.includes('sell.html') && !previousPage.includes('products.html') && !previousPage.includes('index.html')) {
+if (!previousPage.includes('product-info.html') && !previousPage.includes('category-info.html') && !previousPage.includes('my-profile.html') && !previousPage.includes('login.html') && !previousPage.includes('categories.html') && !previousPage.includes('cart.html') && !previousPage.includes('sell.html') && !previousPage.includes('products.html') && !previousPage.includes('index.html')) {
     location.href = 'login.html';
 }
 
