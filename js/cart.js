00a645f51a86792cc7755b205c5457c4b8d6b4cb
Marcos Viera <marcos.viera.1899@gmail.com>
2020-10-09 14:48:26 -0300

    Modificando carrito y actualizando subtotales

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,12 @@
 let contenedor = document.getElementById('cart-container');
+let subtotal = document.getElementById('subtotal');
 
 function showCart(array){
    let cartItems = array.articles;
    htmlToAppend ="";
+   let allSubtotal = 0;
    for(item in cartItems){
-       let totalCost = cartItems[item].count * cartItems[item].unitCost;
-       if(cartItems[item].currency == "USD"){
-           totalCost = totalCost * 40;
-       }
+        allSubtotal += productSubTotal(cartItems);
         htmlToAppend +=`
                     <tr>
                         <td class="col">
@@ -20,27 +19,75 @@ function showCart(array){
                         </div>
                         </td>
                         <td class="col" style="text-align: center">
-                        <input type="email" class="form-control" value="${cartItems[item].count}">
+                        <input type="number" min="1" class="form-control" value="${cartItems[item].count}" id="productCount${item}">
                         </td>
-                        <td class="col text-center"><strong>$${cartItems[item].unitCost} ${cartItems[item].currency}</strong></td>
-                        <td class="col text-center"><strong>$${totalCost} UYU</strong></td>
+                        <td class="col text-center"><strong><span id="productCost${item}">${cartItems[item].unitCost}</span> ${cartItems[item].currency}</strong></td>
+                        <td class="col text-center"><strong>$<span id="productSubtotal${item}">${productSubTotal(cartItems)}</span> UYU</strong></td>
                         <td class="col">
                         <button type="button" class="btn btn-danger">
                         <i class="far fa-trash-alt"></i> Borrar
                         </button></td>
                     </tr>
         `
+    
+        
    }
-contenedor.innerHTML += htmlToAppend;
+   contenedor.innerHTML = htmlToAppend;
+   subtotal.innerHTML += allSubtotal;
+   updateSubtotal(cartItems);
+   
 }
+
+function productSubTotal(cartItems){
+    // funcion que devuelva el costo total de un producto segun la cantidad
+    let totalCost = cartItems[item].count * cartItems[item].unitCost;
+        if(cartItems[item].currency == "USD"){
+            totalCost = totalCost * 40;
+        }
+     return totalCost;   
+ };
+
+ //Función que actualiza el subtotal x producto y el subtotal del carrito al cambiar la cantidad de productos seleccionados
+ function updateSubtotal(cartItems){
+    
+     for(item in cartItems){
+        let suma = 0;
+         let count = document.getElementById('productCount'+item);
+         let cost = document.getElementById('productCost'+item);
+         let costNumber = parseInt(cost.innerHTML, 10);
+         let productSubtotal = document.getElementById('productSubtotal'+item);  
+
+         if(cartItems[item].currency == "USD"){
+            costNumber = costNumber * 40;
+        }
+         
+         count.addEventListener('change', function(){
+            let countNumber = count.value;
+            suma = costNumber * countNumber;
+            productSubtotal.innerHTML = suma;
+            updateCartSubtotal(cartItems);
+        });
+         
+     }
+      
+ }
+
+function updateCartSubtotal(cartItems){
+    let acumulador = 0;
+     for(item in cartItems){
+         let productSubtotal = document.getElementById('productSubtotal'+item).innerHTML;
+        let subtotalNumber = parseInt(productSubtotal, 10);
+        acumulador += subtotalNumber;
+        
+     }
+     subtotal.innerHTML = "$"+acumulador;
+ }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
-
 document.addEventListener("DOMContentLoaded", function(e){
-getJSONData(CART_INFO_URL)
+getJSONData(CART_INFO_2)
     .then(response=>{
         if (response.status === "ok"){
             let cartInfo = response.data;
